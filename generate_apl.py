9842a8096c81aa261fb4ac6d0b4e1fbdf4322ee9
gastank <42421688+gastank@users.noreply.github.com>
2021-03-31 13:41:10 -0700

    use inc file for update_apl.py

--- generate_apl.py
+++ generate_apl.py
@@ -6,15 +6,13 @@ parser.add_argument('simcpath', type=str, help='path to SimC root (include \\sim
 args = parser.parse_args()
 simc_path = args.simcpath
 
-cpp_path = os.path.join(simc_path, 'engine', 'class_modules', 'sc_druid.cpp')
-
 apl_lists = {}
 
 with open('balance.txt', 'r') as apl_file:
     temp_comment=""
     for line in apl_file:
         if line.startswith('####'):
-            temp_comment=line.strip('# \n')
+            temp_comment = line.strip('# \n')
         if line.startswith('actions'):
             apl = 'default'
 
@@ -34,47 +32,22 @@ with open('balance.txt', 'r') as apl_file:
             apl_lists[apl].append((suf,temp_comment))
             temp_comment=""
 
-marker_start = '### BALANCE_APL_START ###'
-marker_end = '### BALANCE_APL_END ###'
+with open(os.path.join(simc_path, 'engine', 'class_modules', 'apl', 'balance_apl.inc'), 'w') as inc:
+    for apl in apl_lists.keys():
+        apl_var = apl
+        if apl_var == 'default':
+            apl_var = 'def'
 
-cpp_old = open(cpp_path, 'r')
-cpp_new = open(cpp_path + '__', 'w')
+        inc.write('action_priority_list_t* ' + apl_var + ' = get_action_priority_list( \"' + apl + '\" );\n')
 
-state = 0
+    for apl in apl_lists.keys():
+        apl_var = apl
+        if apl_var == 'default':
+            apl_var = 'def'
 
-for line in cpp_old:
-    if marker_end in line:
-        state = 0
-
-    if state == 0:
-        cpp_new.write(line)
-
-    if marker_start in line:
-        state = 2
-
-    if state == 2:
-        state = 1
-
-        for apl in apl_lists.keys():
-            apl_var = apl
-            if apl_var == 'default':
-                apl_var = 'def'
-
-            cpp_new.write('  action_priority_list_t* ' + apl_var + ' = get_action_priority_list( \"' + apl + '\" );\n')
-
-        for apl in apl_lists.keys():
-            apl_var = apl
-            if apl_var == 'default':
-                apl_var = 'def'
-
-            cpp_new.write('\n')
-            for action in apl_lists[apl]:
-                if action[1]:                    
-                    cpp_new.write('  ' + apl_var + '->add_action( \"' + action[0] + '\",\"'+action[1]+'\" );\n')
-                else:
-                    cpp_new.write('  ' + apl_var + '->add_action( \"' + action[0] + '\" );\n')
-
-cpp_new.close()
-cpp_old.close()
-
-os.replace(cpp_path + '__', cpp_path)
+        inc.write('\n')
+        for action in apl_lists[apl]:
+            if action[1]:
+                inc.write(apl_var + '->add_action( \"' + action[0] + '\",\"'+action[1]+'\" );\n')
+            else:
+                inc.write(apl_var + '->add_action( \"' + action[0] + '\" );\n')
