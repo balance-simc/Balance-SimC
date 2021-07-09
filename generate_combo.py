e717d0106e562bba871a255e986a41be53d8f254
gastank <42421688+gastank@users.noreply.github.com>
2021-07-09 14:24:07 -0700

    add venture plan file

--- generate_combo.py
+++ generate_combo.py
@@ -285,7 +285,7 @@ for leg, leg_str in legendaries.items():
                     continue
 
                 if 'message' in status and status['message'] == 'No job found':
-                    sys.exit("The sim got lost :(")
+                    sys.exit("The sim got lost :( {}".format(simID))
 
                 if status['job']['state'] == 'complete':
                     data = requests.get(sim_url + '/data.json')
